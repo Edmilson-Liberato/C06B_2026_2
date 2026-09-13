d97df3e368db870bc15d3f7322fb61bdba55d11d
Edmilson-Liberato <edmilsonliberato10@hotmail.com>
2026-09-13 20:41:43 -0300

    Adcionando Aula 1

---
+++ Aula1/exercicio01_olajava.java/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    static void main() {
+        System.out.println ("Ola Java");
+        System.out.println ("Ola java 2");
+        int x = 10;
+        int y = 10;
+
+        System.out.println( "O resultado é: " + (x +y));
+
+        }
+    }
+
